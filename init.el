88fc6480e5413cdbf661290b06af2dc399152063
TrayserCassa <zonkerderks@googlemail.com>
2017-04-26 08:50:54 +0000

    change maximize window

--- init.el
+++ init.el
@@ -18,7 +18,7 @@
  ;; If you edit it by hand, you could mess it up, so be careful.
  ;; Your init file should contain only one such instance.
  ;; If there is more than one, they won't work right.
- '(initial-frame-alist (quote ((fullscreen . maximized))))
+
  '(package-selected-packages
    (quote
     (yasnippet yaml-mode use-package suscolors-theme sqlite s pyvenv magit json-mode highlight-indentation helm find-file-in-project company auto-complete))))
